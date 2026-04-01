2a667b501f308324aa04c7468f0b1e01a7bf77e1
FreLanita <chenshiqi422941259@163.com>
2026-04-01 11:27:16 +0700

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import Components from 'unplugin-vue-components/vite'
 import UnoCSS from 'unocss/vite'
 import Inspect from 'vite-plugin-inspect'
 
-import { GitChangelog, GitChangelogMarkdownSection } from '@nolebase/vitepress-plugin-git-changelog/vite'
 import { PageProperties, PagePropertiesMarkdownSection } from '@nolebase/vitepress-plugin-page-properties/vite'
 import { ThumbnailHashImages } from '@nolebase/vitepress-plugin-thumbnail-hash/vite'
 
@@ -21,18 +20,6 @@ export default defineConfig(async () => {
     },
     plugins: [
       Inspect(),
-      GitChangelog({
-        repoURL: () => githubRepoLink,
-      }),
-      GitChangelogMarkdownSection({
-        getContributorsTitle: (): string => {
-          return '贡献者'
-        },
-        excludes: [
-          'toc.md',
-          'index.md',
-        ],
-      }),
       PageProperties(),
       PagePropertiesMarkdownSection({
         excludes: [
